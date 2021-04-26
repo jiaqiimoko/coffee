7243e3cab887d2c9b06426ee5fc070b0fc36f0d5
jiaqi <wujiaqi123>
2021-04-26 19:45:05 +0800

    123

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,39 +2,85 @@ const app = getApp()
 
 Page({
   data:{
-    nowPage: "home",
-    nowIndex: 0,
+    nowPage: "personal",
+    nowIndex: 4,
     tabBar:[
       {
         "iconClass":"iconfont icon-shouye",
         "text":"首页",
-        "tapFunction":"toFirst",
-        "active":"active"
+        "tapFunction":"toHome",
+        "active":""
       },
       {
         "iconClass":"iconfont icon-ziyuan",
         "text":"菜单",
-        "tapFunction": "toSecond",
+        "tapFunction": "toMenu",
         "active": ""
       },
       {
         "iconClass":"iconfont icon-dingdan",
         "text":"订单",
-        "tapFunction": "toSecond",
+        "tapFunction": "toOrders",
         "active": ""
       },
       {
         "iconClass":"iconfont icon-gouwuche1",
         "text":"购物车",
-        "tapFunction": "toSecond",
+        "tapFunction": "toCar",
         "active": ""
       },
       {
         "iconClass":"iconfont icon-geren",
         "text":"个人",
-        "tapFunction": "toSecond",
-        "active": ""
+        "tapFunction": "toPersonal",
+        "active": "active"
       }
-    ]
+    ],
+    isFixedTop:false
+  },
+  /**
+    * 监听页面滑动事件
+    */
+  onPageScroll(e) {
+    var scrollTop = parseInt(e.scrollTop); //滚动条距离顶部高度
+    //判断'滚动条'滚动的距离 和 '元素在初始时'距顶部的距离进行判断
+    var isSatisfy = scrollTop >= this.data.navbarInitTop ? true : false;
+    //为了防止不停的setData, 这儿做了一个等式判断。 只有处于吸顶的临界值才会不相等
+    if (this.data.isFixedTop === isSatisfy) {
+      return false;
+    }
+    this.setData({
+      isFixedTop: isSatisfy
+    });
+  },
+  toHome(){
+    this.setData({
+      nowPage:"home",
+      nowIndex: 0
+    })
+  },
+  toMenu() {
+    this.setData({
+      nowPage: "menu",
+      nowIndex: 1
+    })
+  },
+  toOrders() {
+    this.setData({
+      nowPage: "orders",
+      nowIndex: 2
+    })
+  },
+  toCar() {
+    this.setData({
+      nowPage: "car",
+      nowIndex: 3
+    })
+  },
+  toPersonal() {
+    this.setData({
+      nowPage: "personal",
+      nowIndex: 4
+    })
   }
 })
--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,66 +1,28 @@
 // pages/personal/personal.js
-Page({
-
-  /**
-   * 页面的初始数据
-   */
+Component({
+  properties:{
+    isFixedTop:{
+      type: Boolean,
+      value: false
+    }
+  },
   data: {
-
+    navbarInitTop:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-
+  ready() {
+    if (this.data.navbarInitTop == 0) {
+      //获取节点距离顶部的距离
+      wx.createSelectorQuery().in(this).select('#navbar').boundingClientRect((rect) => {
+        if (rect && rect.top > 0) {
+        var navbarInitTop = parseInt(rect.top);
+        this.setData({
+          navbarInitTop: navbarInitTop
+        });
+        }
+      }).exec();
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
